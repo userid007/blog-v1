80272ead46acde1063bfbadc1e9ec5d0882e81e1
userid007 <shindeakshay7499@gmail.com>
2020-07-21 23:32:39 +0530

    new global posts array which store objects

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pelle
 const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
 
 const app = express();
+const posts = [];
 
 app.set("view engine", "ejs");
 
@@ -16,6 +17,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 app.get("/", function (req, res) {
+	console.log(posts);
 	res.render("home", { homeStartingContent: homeStartingContent });
 });
 
@@ -32,8 +34,8 @@ app.get("/compose", function (req, res) {
 });
 
 app.post("/compose", function (req, res) {
-	const post = req.body;
-	res.redirect("/compose");
+	posts.push(req.body);
+	res.redirect("/");
 });
 
 
