197d61e3919f0b809f24e6794a68887431215e43
userid007 <shindeakshay7499@gmail.com>
2020-07-21 23:51:22 +0530

    console log posts array from home route

--- app.js
+++ app.js
@@ -17,8 +17,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 app.get("/", function (req, res) {
-	console.log(posts);
-	res.render("home", { homeStartingContent: homeStartingContent });
+	res.render("home", { homeStartingContent: homeStartingContent , posts: posts });
 });
 
 app.get("/about", function (req, res) {
